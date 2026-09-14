e22ba3b11e5598b1cccbdc67939eeed3f313f1d9
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2026-09-14 19:40:20 -0400

    Implement recursive functions for building binary search tree

--- balanced-binary-search-tree.js
+++ balanced-binary-search-tree.js
@@ -1,3 +1,28 @@
+class Tree {
+  constructor(arr) {
+    this.root = this.#buildTree(arr);
+  }
+
+  #buildTree(arr) {
+    const arrClean = [...new Set(arr.sort())];
+
+    return this.#createBST(arrClean, 0, arrClean.length - 1);
+  }
+
+  #createBST(arr, start, end) {
+    if (start > end) {
+      return null;
+    }
+    const mid = Math.floor((start + end) / 2);
+    const root = new Node(arr[mid]);
+
+    root.left = this.#createBST(arr, start, mid - 1);
+    root.right = this.#createBST(arr, mid + 1, end);
+
+    return root;
+  }
+}
+
 class Node {
   constructor(value) {
     this.value = value;
@@ -5,3 +30,5 @@ class Node {
     this.right = null;
   }
 }
+
+export { Tree };
