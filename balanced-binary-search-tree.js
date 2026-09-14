3c01b0d632d9b4d2a038058579ad525167e80f18
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2026-09-14 19:56:10 -0400

    Use provided prettyPrint function to visually display the tree structure in the console

--- balanced-binary-search-tree.js
+++ balanced-binary-search-tree.js
@@ -23,6 +23,16 @@ class Tree {
   }
 }
 
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null || node === undefined) {
+    return;
+  }
+
+  prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+  prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+};
+
 class Node {
   constructor(value) {
     this.value = value;
