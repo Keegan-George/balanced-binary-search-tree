ea089a7d6b8be3ca2b0175b9eba7aecb6cc118b0
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2026-09-16 16:53:32 -0400

    Add unit tests for depth function. Group tests into logical sections

--- balanced-binary-search-tree.test.js
+++ balanced-binary-search-tree.test.js
@@ -10,7 +10,7 @@ describe("Positive cases", () => {
     tree = new Tree(NODE_DATA);
   });
 
-  describe("includes() tests", () => {
+  describe("Verify all nodes in tree", () => {
     test.each(NODE_DATA)(
       "Return true for value in tree: includes(%i) => true",
       (n) => {
@@ -18,28 +18,42 @@ describe("Positive cases", () => {
       },
     );
 
-    test("Return false for value not in tree", () => {
-      expect(tree.includes(400)).toBe(false);
+    test.each([
+      [1, 2],
+      [3, 1],
+      [5, 2],
+      [7, 0],
+      [9, 2],
+      [11, 1],
+      [13, 2],
+    ])("Return depth of value in tree: depth(%i) => %i", (n, expected) => {
+      expect(tree.depth(n)).toBe(expected);
     });
   });
 
-  describe("insert() tests", () => {
+  describe("insert nodes", () => {
     test("Can insert new smallest value", () => {
       expect(tree.includes(0)).toBe(false);
+      expect(tree.depth(0)).toBeUndefined();
       tree.insert(0);
       expect(tree.includes(0)).toBe(true);
+      expect(tree.depth(0)).toBe(3);
     });
 
     test("Can insert value in middle", () => {
       expect(tree.includes(8)).toBe(false);
+      expect(tree.depth(8)).toBeUndefined();
       tree.insert(8);
       expect(tree.includes(8)).toBe(true);
+      expect(tree.depth(8)).toBe(3);
     });
 
     test("Can insert new largest value", () => {
       expect(tree.includes(15)).toBe(false);
+      expect(tree.depth(15)).toBeUndefined();
       tree.insert(15);
       expect(tree.includes(15)).toBe(true);
+      expect(tree.depth(15)).toBe(3);
     });
   });
 });
