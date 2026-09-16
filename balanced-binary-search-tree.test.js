69fb854a3f0fdfef3e1d893306cfd0d677afb9a1
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2026-09-16 08:28:42 -0400

    Add unit tests for insert function

--- balanced-binary-search-tree.test.js
+++ balanced-binary-search-tree.test.js
@@ -3,7 +3,7 @@ import { test, expect, describe, beforeEach } from "@jest/globals";
 
 let tree;
 
-const NODE_DATA = [1, 2, 3, 4, 5, 6, 7];
+const NODE_DATA = [1, 3, 5, 7, 9, 11, 13];
 
 describe("Positive cases", () => {
   beforeEach(() => {
@@ -19,7 +19,27 @@ describe("Positive cases", () => {
     );
 
     test("Return false for value not in tree", () => {
-      expect(tree.includes(500)).toBe(false);
+      expect(tree.includes(400)).toBe(false);
+    });
+  });
+
+  describe("insert() tests", () => {
+    test("Can insert new smallest value", () => {
+      expect(tree.includes(0)).toBe(false);
+      tree.insert(0);
+      expect(tree.includes(0)).toBe(true);
+    });
+
+    test("Can insert value in middle", () => {
+      expect(tree.includes(8)).toBe(false);
+      tree.insert(8);
+      expect(tree.includes(8)).toBe(true);
+    });
+
+    test("Can insert new largest value", () => {
+      expect(tree.includes(15)).toBe(false);
+      tree.insert(15);
+      expect(tree.includes(15)).toBe(true);
     });
   });
 });
